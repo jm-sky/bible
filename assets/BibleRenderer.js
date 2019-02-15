5fbdb336b3b0d827fcdd42ad0deb827cf6ec17ca
Jan Madeyski <madeyskij@sklodowscy.local>
2019-02-15 21:26:54 +0100

    List, ReadAll, Search Highlight

--- assets/BibleRenderer.js
+++ assets/BibleRenderer.js
@@ -16,7 +16,7 @@ BibleRenderer.read = function(options = {}) {
   })
   .then(content => {
     console.log('[BibleRenderer] READ content:', content);
-    let book = Object.keys(content.books)[0];
+    let book = options.readAll || Object.keys(content.books)[0];
     BibleRenderer.bible = content;
     $title.text(content.title);
     $publisher.text(content.publisher);
@@ -28,7 +28,9 @@ BibleRenderer.read = function(options = {}) {
 BibleRenderer.drawMenu = function() {
   console.log('[BibleRenderer.drawMenu]');
   $menu.html('');
-  $menu.append('<div class="col-md-1 col-form-label">Wybierz księgę</div><div class="col-md-11 select-wrapper"></div>');
+  $menu.append('<div class="col-md-1 col-form-label">Wybierz księgę</div>');
+  $menu.append('<div class="col-md-10 select-wrapper"></div>');
+  $menu.append($showAll);
   $menu.find('.select-wrapper').append($select);
   $select.change(function() { BibleRenderer.drawContent($(this).val() ); });
 
@@ -38,20 +40,59 @@ BibleRenderer.drawMenu = function() {
   });
 };
 //---------------------------------------------------------
-BibleRenderer.drawContent = function(book) {
-  console.log('[BibleRenderer.drawContent]', book);
-  let chapters = BibleRenderer.bible.books[book];
+BibleRenderer.drawContent = function(bookRange) {
+  console.log('[BibleRenderer.drawContent]', bookRange);
+  let range = [],
+      $list = $('<div class="m-1 p-1 border rounded shadow-sm text-muted text-center"></div>');
+  range = bookRange === true ? Object.keys(BibleRenderer.bible.books) : [bookRange];
   
-  $select.val(book);
-  $book.text(book);
   $chapters.html('');
+  $chapters.append($list);
 
-  Object.values(chapters).forEach((verses, chapter) => {
-    $chapters.append($(`<h3 id="chapter_${chapter + 1}" class="mt-2">Rozdział ${chapter + 1}</h3>`));
-    verses.forEach(verse => {
-      $chapters.append(`<span><small class="MsoSubtleReference text-muted">(${verse.lp})</small>&nbsp;${verse.text}</span> `);
+  range.forEach((book, index) => {
+    let chapters = BibleRenderer.bible.books[book],
+        $book = $(`<h2 id="book_${index}" class="title text-center mt-3">${book}</h2>`);
+  
+    $select.val(book);
+    $chapters.append($book);
+    if (bookRange === true) {
+      $list.append(`${index > 0 ? ' | ' : ''}<a href="#book_${index}" class="m-1 d-inline-block">${book}</a>`);
+    }
+
+    Object.values(chapters).forEach((verses, chapter) => {
+      if (bookRange !== true) {
+        $list.append(`${chapter > 0 ? ' | ' : ''}<a href="#chapter_${chapter + 1}" class="m-1 d-inline-block">${chapter + 1}</a>`);
+      }
+      $chapters.append($(`<h3 id="chapter_${chapter + 1}" class="mt-2">Rozdział ${chapter + 1}</h3>`));
+      verses.forEach(verse => {
+        $chapters.append(`<span id="chapter_${chapter + 1}_verse_${verse.lp}"><small class="MsoSubtleReference text-muted">(${verse.lp})</small>&nbsp;${verse.text}</span> `);
+      });
     });
   });
+
+};
+//---------------------------------------------------------
+BibleRenderer.addSearchResult = function(book, chapter, verse, string) {
+  let $item = $(`<div class="px-2 py-1 m-1 border rounded"></div>`),
+      $link = $(`<a href="#chapter_${chapter}" class="float-right small text-muted">${book} ${chapter}:${verse.lp}</a>`),
+      pattern = new RegExp(`(${string})`, 'i');
+
+    $link.click(function() {
+      BibleRenderer.drawContent(book);
+      $modal.modal('hide');
+      let $linkedVerse = $(`#chapter_${chapter}_verse_${verse.lp}`);
+      $linkedVerse.css({'background-color': '#ffa'}).delay(1000).queue(function(next) {
+        $linkedVerse.css({'background-color': 'unset'});
+        next();
+      });
+    });
+
+    $item.append(`${verse.text.replace(pattern, '<b>$1</b>')}`);
+    $item.append(`<div class="clearfix"></div>`);
+    $item.append($link);
+    $item.append(`<div class="clearfix"></div>`);
+    $results.append($item);
+    $modal.modal('show');
 };
 //---------------------------------------------------------
 BibleRenderer.search = function(string) {
@@ -63,32 +104,15 @@ BibleRenderer.search = function(string) {
   $results.html('');
 
   Object.keys(BibleRenderer.bible.books).forEach(book => {
-    console.log('book:', book);
     Object.keys(BibleRenderer.bible.books[book]).forEach(c => {
       let chapter = BibleRenderer.bible.books[book][c];
-      console.log('book:', c, chapter);
       chapter.forEach(v => {
         if (v.text.toLowerCase().includes(string.toLowerCase())) {
-          let $item = $(`<div class="px-2 py-1 m-1 border rounded"></div>`),
-              $link = $(`<a href="#chapter_${c}" class="float-right small text-muted">${book} ${c}:${v.lp}</a>`),
-              pattern = new RegExp(`(${string})`, 'i');
-
-          $link.click(function() {
-            BibleRenderer.drawContent(book);
-            $modal.modal('hide');
-          });
-
-          $item.append(`${v.text.replace(pattern, '<b>$1</b>')}`);
-          $item.append(`<div class="clearfix"></div>`);
-          $item.append($link);
-          $item.append(`<div class="clearfix"></div>`);
-          $results.append($item);
-          $modal.modal('show');
+          BibleRenderer.addSearchResult(book, c, v, string);
         }
       });
     });
   });
-
 
 };
 //---------------------------------------------------------
@@ -100,7 +124,6 @@ BibleRenderer.render = function(options = {}) {
       $menu = $('<div class="menu my-1 border p-2 row m-1 rounded shadow-sm"></div>'),
       $select = $(`<select class="form-control"></select>`),
       $content = $('<div class="content text-justify"></div>'),
-      $book = $(`<h2 class="title text-center mt-3"></h2>`),
       $chapters = $(`<div class="chapters"></div>`),
       $versions = $('<div class="versions p-1 m-1 shadow-sm border rounded" style="position: fixed; top: 0; left: 0; max-width: 48vw;"></div>'),
       $version = $('<select placeholder="Wersja" class="form-control form-control-sm"></select>'),
@@ -112,7 +135,8 @@ BibleRenderer.render = function(options = {}) {
       $modalDialog = $('<div class="modal-dialog modal-lg"></div>'),
       $modalContent = $('<div class="modal-content p-1"></div>'),
       $modalClose = $('<a href="#" class="btn btn-primary text-light m-1" data-dismiss="modal"><i class="fas fa-times"></i> Zamknij</a>'),
-      $results = $('<div class="results"></div>');
+      $results = $('<div class="results"></div>'),
+      $showAll = $('<a href="#" class="col-md-1 btn btn-light btn-sm"><i class="fas fa-book"></i> Wszystkie</a>');
 
     $wrapper.html('');
     $wrapper.append($title);
@@ -132,13 +156,17 @@ BibleRenderer.render = function(options = {}) {
     $bottomRight.append($scrollTop);
     $bottomRight.css({'z-index': 10000});
     $scrollTop.click(() =>  $modal.scrollTop(0));
-    $content.append($book);
     $content.append($chapters);
       
   $search.keyup(function(e) {
     if (e.which == 13) {
       BibleRenderer.search($search.val());
     }
+  });
+
+  $showAll.click(function() {
+    options.readAll = true;
+    BibleRenderer.read(options);
   });
 
   if (options.urls) {
