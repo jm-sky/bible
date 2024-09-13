b2611a5cb05b3d945918dfca5b75a95c1ac18e18
Jan Madeyski <jan.madeyski@gmail.com>
2024-09-13 13:57:32 +0200

    fixes

--- src/components/bLoader.vue
+++ src/components/bLoader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute left-0 top-0 grid h-screen w-screen items-center justify-center bg-white/30">
+  <div class="absolute left-0 top-0 grid h-screen w-screen items-center justify-center bg-white/30 backdrop-blur-xs">
     <div>
       <i class="fa-sync fa-solid fa-spin fa-3x opacity-50" />
     </div>
